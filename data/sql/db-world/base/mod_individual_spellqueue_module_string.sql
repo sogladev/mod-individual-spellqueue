af1afb792120d2e66838bce3b3362e66fafb3591
Jelle Meeus <sogladev@gmail.com>
2025-01-10 16:12:49 +0100

    update config and commands (#4)
    * update strings and config
    
    use worldserver.conf default window
    
    min,max errors to range
    
    * handle .set 0 as disable

--- data/sql/db-world/base/mod_individual_spellqueue_module_string.sql
+++ data/sql/db-world/base/mod_individual_spellqueue_module_string.sql
@@ -2,16 +2,15 @@
 SET @MODULE_STRING := 'individual-spellqueue';
 DELETE FROM `module_string` WHERE `module` = @MODULE_STRING;
 INSERT INTO `module_string` (`module`, `id`, `string`) VALUES
-(@MODULE_STRING, 1, 'Your current spellqueue window is {}.'),
-(@MODULE_STRING, 2, 'The maximum spellqueue window is {}.'),
-(@MODULE_STRING, 3, 'The minimum spellqueue window is {}.'),
-(@MODULE_STRING, 4, 'You have updated your spellqueue window to {}.'),
-(@MODULE_STRING, 5, 'You have disabled your spellqueue.'),
-(@MODULE_STRING, 6, 'You have enabled your spellqueue with a window of {}.'),
-(@MODULE_STRING, 7, 'You have restored your spellqueue window to the default value of {}.'),
-(@MODULE_STRING, 8, 'The individual-spellqueue-module is disabled.'),
-(@MODULE_STRING, 9, 'You have enabled spellqueue latency compensation with a window of {}.'),
-(@MODULE_STRING, 10, 'You have disabled spellqueue latency compensation with a window of {}.');
+(@MODULE_STRING, 1, 'Your spellqueue window is {}.'),
+(@MODULE_STRING, 2, 'The spellqueue window must be between {} and {}.'),
+(@MODULE_STRING, 3, 'You have updated your spellqueue window to {}.'),
+(@MODULE_STRING, 4, 'You have disabled your spellqueue.'),
+(@MODULE_STRING, 5, 'You have enabled your spellqueue with a window of {}.'),
+(@MODULE_STRING, 6, 'You have restored your spellqueue window to the default value of {}.'),
+(@MODULE_STRING, 7, 'The individual-spellqueue-module is disabled.'),
+(@MODULE_STRING, 8, 'You have enabled spellqueue latency compensation with a window of {}.'),
+(@MODULE_STRING, 9, 'You have disabled spellqueue latency compensation with a window of {}.');
 
 -- 'deDE', 'ruRU', 'ruRU', 'koKR', 'frFR', 'zhCN', 'zhTW', 'esES', 'esMX',
 DELETE FROM `module_string_locale` WHERE `module` = @MODULE_STRING;
